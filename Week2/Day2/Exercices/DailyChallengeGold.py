65d554293d1cca578aa65e504f7562c8c74bfc72
Avigdor <avigdorpartouche@gmail.com>
2023-09-04 18:03:14 +0200

    week 2 day 2 Daily Challenge Gold

--- Week2/Day2/Exercices/DailyChallengeGold.py
+++ Week2/Day2/Exercices/DailyChallengeGold.py
@@ -1,7 +1,20 @@
-# ******************* #
-# Here I test the codes
-# ******************* #
 
+#     Ask the user for their birthdate (specify the format, for example: DD/MM/YYYY).
+#     Display a little cake as seen below:
+
+#        ___iiiii___
+#       |:H:a:p:p:y:|
+#     __|___________|__
+#    |^^^^^^^^^^^^^^^^^|
+#    |:B:i:r:t:h:d:a:y:|
+#    |                 |
+#    ~~~~~~~~~~~~~~~~~~~
+
+# The number of candles on the cake should be the last number of the users age, if they are 53, then add 3 candles.
+
+# Bonus : If they were born on a leap year, display two cakes !
+
+# from datetime import date
 bd = input("What is your birthday? (in the format DD/MM/YYYY)\n")
 today = "04/09/2023"
 today_list = today.split("/")
