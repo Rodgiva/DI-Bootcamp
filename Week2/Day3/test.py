8d3ed0287255cabf43f626dfc3fe7d66c217bc42
Avigdor <avigdorpartouche@gmail.com>
2023-09-05 15:33:10 +0200

    week 2 day 3

--- Week2/Day3/test.py
+++ Week2/Day3/test.py
@@ -1,22 +1,18 @@
-def encrypts(msg):
-    encripted_msg = ""
-    for char in msg:
-        shifted_char = chr(ord(char)+3)
-        encripted_msg += shifted_char
-    return encripted_msg
+def encrypts_decrypts(msg, opt):
+    res_msg = ""
+    to_inc = 0
+    if opt == "1":
+        to_inc = 3
+    elif opt == "2":
+        to_inc = -3
 
-def decrypts(msg):
-    decripted_msg = ""
     for char in msg:
-        shifted_char = chr(ord(char)-3)
-        decripted_msg += shifted_char
-    return decripted_msg
+        shifted_char = chr(ord(char)+to_inc)
+        res_msg += shifted_char
+    return res_msg
 
 user_msg = input("Please write your message\n")
 
 choose = input("Do you want to encrypts or decrypts ? If you want to encrypts, write '1'. If you want to decrypts, write '2'\n")
 
-if choose == "1":
-    print(encrypts(user_msg))
-elif choose == "2":
-    print(decrypts(user_msg))
+print(encrypts_decrypts(user_msg, choose))
