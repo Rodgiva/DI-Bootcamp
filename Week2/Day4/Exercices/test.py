c77de2bd9885803b8ef4f045fd9ecf4d06bd22b9
Avigdor <avigdorpartouche@gmail.com>
2023-09-06 15:54:39 +0200

    week 2 day 4 Exercices

--- Week2/Day4/Exercices/test.py
+++ Week2/Day4/Exercices/test.py
@@ -1,50 +1,5 @@
-data = [
-    {
-        "question": "What is Baby Yoda's real name?",
-        "answer": "Grogu"
-    },
-    {
-        "question": "Where did Obi-Wan take Luke after his birth?",
-        "answer": "Tatooine"
-    },
-    {
-        "question": "What year did the first Star Wars movie come out?",
-        "answer": "1977"
-    },
-    {
-        "question": "Who built C-3PO?",
-        "answer": "Anakin Skywalker"
-    },
-    {
-        "question": "Anakin Skywalker grew up to be who?",
-        "answer": "Darth Vader"
-    },
-    {
-        "question": "What species is Chewbacca?",
-        "answer": "Wookiee"
-    }
-]
+def get_full_name(first_name, last_name, middle_name = ""):
+    name = f"{first_name} {middle_name} {last_name}"
+    print(name)
 
-wrong_answers = []
-def starwarsquizz():
-    correct = 0
-    incorrect = 0
-    for i in range(len(data)):
-        user_answer = input(f"{data[i]['question']}\n")
-        if user_answer == data[i]["answer"]:
-            correct += 1
-        else:
-            incorrect += 1
-            wrong_answers.append((i, user_answer))
-        print("Noted.")
-    quizz_correction(correct, incorrect)
-
-def quizz_correction(correct, incorrect):
-    print(f"Quizz is over! Let check you answers.\nYou did {correct} correct answers for {incorrect} incorrect answers")
-    for i in range(len(wrong_answers)):
-        print(f"In the question {data[i]['question']} you answered {wrong_answers[i][1]}, and the correct answer is {data[wrong_answers[i][0]]['answer']}")
-    if incorrect > 3:
-        print("Lets try again!")
-        starwarsquizz()
-
-starwarsquizz()
+get_full_name(first_name="Bob", last_name="machin")
