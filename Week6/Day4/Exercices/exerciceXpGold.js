2f12daf510fc242169ebaf361e71b37ef5223cdb
Avigdor <avigdorpartouche@gmail.com>
2023-10-25 16:01:22 +0300

    Week 6 Day 4

--- Week6/Day4/Exercices/exerciceXpGold.js
+++ Week6/Day4/Exercices/exerciceXpGold.js
@@ -44,7 +44,6 @@ const newArray = arrayNum.map((num, i) => {
 // ==> i: 0, 1, 2, 3, 4, 5
 // *********************************
 
-*/
 
 // Exercise 4 : Nested arrays
 // Instructions
@@ -70,3 +69,96 @@ console.log(greeting.toString().replaceAll(",", " "));
 // 4. Turn the trapped number 3 const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]] into: [3]
 const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
 console.log([trapped.toString()]);
+
+// *********************************
+*/
+
+// Exercise 1: Sum elements
+// Instructions
+
+// 1. Write a JavaScript program to find the sum of all elements in an array.
+const nbs = [10, 5, 6, 17, 7];
+console.log(nbs.reduce((acc, val) => acc + val));
+
+// *********************************
+
+// Exercise 2 : Remove Duplicates
+// Instructions
+
+// 1. Write a JavaScript program to remove duplicate items in an array.
+const dupArr = [6, 6, 8, 7, 8, 5, 6, 2];
+let removeDuplicate = (arr) => arr.filter((val, i) => arr.indexOf(val) == i);
+console.log(removeDuplicate(dupArr));
+
+// *********************************
+
+// Exercise 3 : Remove certain values
+// Instructions
+
+// 1. Write a JavaScript function to remove: null, 0, "", false, undefined and NaN values from an array.
+
+// Sample array : [NaN, 0, 15, false, -22, '',undefined, 47, null]
+// Expected result : [15, -22, 47]
+
+const sampleArr = [NaN, 0, 15, false, -22, "", undefined, 47, null];
+const filterRef = [null, 0, "", false, undefined]
+console.log(
+  sampleArr.filter(element => !filterRef.includes(element) && !isNaN(element))
+);
+
+// *********************************
+
+// Exercise 4 : Repeat please !
+// Instructions
+
+// 1. Write a JavaScript function to concatenate a given string n times (default is 1).
+// Create the repeat function yourself:
+
+let repeat = (word, n) => {
+    let res = ""
+    for (let i = 0; i < n; i++) {
+        res += word
+    }
+    return res
+}
+
+console.log(repeat('Ha!',3));
+// "Ha!Ha!Ha!"
+
+// *********************************
+
+// Exercise 5 : Turtle & Rabbit
+
+// For this exercise, look at the lesson More JS methods.
+
+// Using this code :
+
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+
+// 1. Line up the Turtle and the Rabbit at the start line.
+//     Expected Output:
+
+//         When you write:
+
+//         console.log(startLine);
+//         console.log(turtle);
+//         console.log(rabbit);
+
+//         It should look like this:
+
+//         '     ||<- Start line'
+//         '       🐢'
+//         '       🐇'
+
+const startIndex = startLine.indexOf("<")
+turtle = turtle.padStart(startIndex)
+rabbit = rabbit.padStart(startIndex)
+
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+// 2. What happens when you run turtle = turtle.trim().padEnd(9, '='); ?
+//  ==> '🐢========='
