fc5c8179f936544a01f919c89c2c1d2951276163
Avigdor <avigdorpartouche@gmail.com>
2023-11-14 16:30:32 +0200

    week 9 day 1

---
+++ Week9/Day1/Exercices/DailyChallenge/myapp/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Carousel from "./Carousel.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <Carousel />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
